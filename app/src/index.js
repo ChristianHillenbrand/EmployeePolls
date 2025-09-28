8d2647d988dfe2eaa139f98451a920e490518ebb
Christian Hillenbrand <christian.hillenbrand@vitronic.de>
2025-09-28 18:12:14 +0200

    refactor: move redux store creation to separate file

--- app/src/index.js
+++ app/src/index.js
@@ -2,13 +2,9 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from "./components/App";
-import reducer from "./reducers";
-import middleware from "./middleware";
-
-const store = createStore(reducer, middleware);
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
